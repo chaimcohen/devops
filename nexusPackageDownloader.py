969f331d93c01c80be9a1cac25024f473e9d62dc
pini cohen <miko579@gmail.com>
2022-12-05 21:00:05 +0200

    nexus package dowloader python

---
+++ nexusPackageDownloader.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import shutil
+import time
+
+os.makedirs("packages", exist_ok=True)
+api_url = "http://34.165.200.87:8081/service/rest/v1/components?repository=pipey_proxy"
+# print(api_url)
+response = requests.get(api_url)
+# print(response)
+x = response.json()
+package_to_upload = ""
+headers = {
+    'accept': 'application/json',
+}
+
+params = {
+    'repository': 'pipyrepo',
+}
+
+
+continueFlag = True
+while continueFlag:
+    for item in x["items"]:
+        for asset in item["assets"]:
+            URL = asset["downloadUrl"]
+            packageName = URL.split("/")[-1]
+            print("downloading " + packageName + "\n")
+            response = requests.get(URL)
+            open("packages/" + packageName, "wb").write(response.content)
+            package_to_upload = packageName
+            files = {'pypi.asset': open("packages/" + package_to_upload, 'rb'), }
+            response = requests.post('http://34.165.200.87:8081/service/rest/v1/components', params=params, headers=headers, files=files, auth=('admin', 'admin'))
+
+    if x["continuationToken"] is not None:
+        api_url = "http://34.165.200.87:8081/service/rest/v1/components?continuationToken=" + x["continuationToken"] + "&repository=pipey_proxy"
+        response = requests.get(api_url)
+        x = response.json()
+    else:
+        continueFlag = False
+# print("sleeping for 60 seconds")
+# time.sleep(120)
+# print("wakey wakey")
+shutil.rmtree('packages', ignore_errors=True)
