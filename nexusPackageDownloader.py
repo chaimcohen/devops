7aa9343fdbd7322f8144423d2be8ff1a6ccf2711
pini cohen <miko579@gmail.com>
2022-12-17 21:25:23 +0200

    validating nexus packages download and upload

--- nexusPackageDownloader.py
+++ nexusPackageDownloader.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import shutil
-import time
+# import time
 
 os.makedirs("packages", exist_ok=True)
 api_url = "http://34.165.200.87:8081/service/rest/v1/components?repository=pipey_proxy"
@@ -27,11 +27,18 @@ while continueFlag:
             packageName = URL.split("/")[-1]
             print("downloading " + packageName + "\n")
             response = requests.get(URL)
+            if response.status_code == 200:
+                print("download succeeded")
+            else:
+                print("failed downloading")
             open("packages/" + packageName, "wb").write(response.content)
             package_to_upload = packageName
             files = {'pypi.asset': open("packages/" + package_to_upload, 'rb'), }
             response = requests.post('http://34.165.200.87:8081/service/rest/v1/components', params=params, headers=headers, files=files, auth=('admin', 'admin'))
-
+            if response.status_code == 204:
+                print("upload succeeded")
+            else:
+                print("failed uploading")
     if x["continuationToken"] is not None:
         api_url = "http://34.165.200.87:8081/service/rest/v1/components?continuationToken=" + x["continuationToken"] + "&repository=pipey_proxy"
         response = requests.get(api_url)
